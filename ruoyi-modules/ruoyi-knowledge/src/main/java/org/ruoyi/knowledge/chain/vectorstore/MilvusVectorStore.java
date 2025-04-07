663c49a1a1b472d8a184d03626f725a7c09148b7
winkey <wwwgto@qq.com>
2025-04-07 15:12:47 +0800

    向量数据库字段调整

--- ruoyi-modules/ruoyi-knowledge/src/main/java/org/ruoyi/knowledge/chain/vectorstore/MilvusVectorStore.java
+++ ruoyi-modules/ruoyi-knowledge/src/main/java/org/ruoyi/knowledge/chain/vectorstore/MilvusVectorStore.java
@@ -67,7 +67,7 @@ public class MilvusVectorStore implements VectorStore {
         FieldType contentField = FieldType.newBuilder()
                 .withName("content")
                 .withDataType(DataType.VarChar)
-                .withMaxLength(1000)
+                .withMaxLength(2000)
                 .build();
         FieldType kidField = FieldType.newBuilder()
                 .withName("kid")
